b5cfe271107bc96cbd321f903a58505213e5e0eb
pawel@4x4.org.pl <pawel@4x4.org.pl@5a043699-637f-544d-1961-cde2c322f4b0>
2013-03-05 21:48:01 +0000

    mpNg_org moved to the branches

--- mpNG_org/config.mk
+++
@@ -1,9 +0,0 @@
-# THIS IS PRE-BETA makefile for megapirateNG build on Windows platform using Cygwin
-#
-# Select 'mega' for the original APM, or 'mega2560' for the V2 APM.
-BOARD=mega2560
-# The communication port used to communicate with the APM.
-PORT=com14
-# Uncomment, if you need a lot of informations on the build process.
-VERBOSE=no
-
