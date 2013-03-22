d22c83c26b78041c6740bbb9b8877cb365c9bad6
pawel@4x4.org.pl <pawel@4x4.org.pl@5a043699-637f-544d-1961-cde2c322f4b0>
2013-03-22 12:50:37 +0000

    PPM: Sonar Disable comments in the config files.

--- branches/PPM/ArduCopter/APM_Config.h
+++ branches/PPM/ArduCopter/APM_Config.h
@@ -18,6 +18,8 @@
 // RC configuration
 
 // PPM_SUM(CPPM) Signal processing
+// WARRING: Sonar is full disabled by default now.
+// But for PPM on PL1 (v1) or PWM Rx mode you have to enable sonar - few lines below.
 #define SERIAL_PPM SERIAL_PPM_ENABLED
 /*
 	SERIAL_PPM_DISABLED
@@ -46,8 +48,9 @@
 #define COPTER_LEDS ENABLED				// Native ArduCopter LEDs
 //#define LED_SEQUENCER ENABLED		// Old Syberian's LED Sequencer, see leds.pde for more info
 
-// PAKU warrning DISABLING sonar makes PL1 unusable for V2 board.
-// if you use v2 board you have to enable sonar here - but you can disable it in MP if not used
+// PAKU warrning DISABLING sonar makes PL1 unusable for V2 board. And PWM mode for all boards.
+// if you use v2 board PWM you have to enable sonar here - but you can disable it in MP if not used
+// comment 1 line below to re-enable.
 #define CONFIG_SONAR DISABLED
 #define MAX_SONAR_RANGE 400
 
