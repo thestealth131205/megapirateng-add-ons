112f940db71dfffcf82c5418367e18928976fc32
pawel@4x4.org.pl <pawel@4x4.org.pl@5a043699-637f-544d-1961-cde2c322f4b0>
2013-06-11 12:18:46 +0000

    Create LatestBETA branch

---
+++ branches/LatestBETA/ArduCopter/revision_conf.h
@@ -0,0 +1,1 @@
+#define THISFIRMWARE "MegaPirateNG Add-Ons V2.9.1 R7.$WCREV$ $WCNOW=%Y/%m/%d$"
---
+++ branches/LatestBETA/config.mk
@@ -0,0 +1,10 @@
+# THIS IS PRE-BETA makefile for megapirateNG build on Windows platform using Cygwin
+#
+# Select 'mega' for the original APM, or 'mega2560' for the V2 APM.
+BOARD=mega2560
+# The communication port used to communicate with the APM.
+PORT=com14
+# Uncomment, if you need a lot of informations on the build process.
+#VERBOSE=no
+SVN_REV=yes
+
